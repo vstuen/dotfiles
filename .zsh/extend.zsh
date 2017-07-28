678fc7ac3c19e7e61988b1361ebc02e75f228c04
Vegard Stuen <vstuen@users.noreply.github.com>
2017-07-29 00:07:07 +0200

    Added basic config for zsh, using antigen

---
+++ .zsh/extend.zsh
@@ -0,0 +1,32 @@
+# Auto-complete for hidden files
+setopt globdots
+
+# fzf integration
+if [[ -d /usr/share/fzf ]]; then
+	source /usr/share/fzf/key-bindings.zsh
+	source /usr/share/fzf/completion.zsh
+fi
+
+
+### FASD
+# fasd init
+eval "$(fasd --init auto)"
+
+# fasd & fzf change directory - jump using 
+# `fasd` if given argument, 
+# filter output of `fasd` using `fzf` else
+unalias z 2> /dev/null
+z() {
+    [ $# -gt 0 ] && fasd_cd -d "$*" && return
+    local dir
+    dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
+}
+
+# fasd & fzf open with vim - open best 
+# matched file using `fasd` if given argument, 
+# filter output of `fasd` using `fzf` else
+v() {
+    [ $# -gt 0 ] && fasd -f -e ${EDITOR} "$*" && return
+    local file
+    file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort +m)" && vim "${file}" || return 1
+}
---
+++ .zsh/include.zsh
@@ -0,0 +1,19 @@
+## For use with antigen
+source $HOME/.zsh/antigen.zsh
+source $HOME/.zsh/antigenrc
+
+## For use with zgen
+# source $HOME/.zgen/zgen.zsh
+# source $HOME/.zsh/zgenrc
+
+# Use grml config
+# source $HOME/.zsh/grml.zsh
+
+## General config
+source $HOME/.zsh/extend.zsh
+source $HOME/.cfg/aliases
+source $HOME/.cfg/env
+
+# Interactive cd plugin
+source $HOME/.zsh/zsh-interactive-cd.plugin.zsh
+
