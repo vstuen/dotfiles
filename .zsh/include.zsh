7b001f3f679735e13205c935581341d84efe817c
Vegard <vstuen@users.noreply.github.com>
2018-08-02 21:29:57 +0200

    Changed grml config
    Also made sure that environment and aliases are preloaded
    prior to loading grml config

--- .zsh/include.zsh
+++ .zsh/include.zsh
@@ -6,6 +6,10 @@
 # source $HOME/.zgen/zgen.zsh
 # source $HOME/.zsh/zgenrc
 
+# Environment and aliases
+source $HOME/.cfg/aliases
+source $HOME/.cfg/env
+
 # Use grml config
 source $HOME/.zsh/grml.zsh
 
@@ -15,6 +19,4 @@ source $HOME/.zsh/zsh-interactive-cd.plugin.zsh
 
 ## General config
 source $HOME/.zsh/extend.zsh
-source $HOME/.cfg/aliases
-source $HOME/.cfg/env
 
