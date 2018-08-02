1538c6ff89fb61baa8f3e91ffffe9fe50e5664b1
Vegard <vstuen@users.noreply.github.com>
2018-08-02 21:20:00 +0200

    Changed to use grml config

--- .zsh/include.zsh
+++ .zsh/include.zsh
@@ -1,19 +1,20 @@
 ## For use with antigen
-source $HOME/.zsh/antigen.zsh
-source $HOME/.zsh/antigenrc
+# source $HOME/.zsh/antigen.zsh
+# source $HOME/.zsh/antigenrc
 
 ## For use with zgen
 # source $HOME/.zgen/zgen.zsh
 # source $HOME/.zsh/zgenrc
 
 # Use grml config
-# source $HOME/.zsh/grml.zsh
+source $HOME/.zsh/grml.zsh
+
+# Interactive cd plugin
+source $HOME/.zsh/zsh-interactive-cd.plugin.zsh
+
 
 ## General config
 source $HOME/.zsh/extend.zsh
 source $HOME/.cfg/aliases
 source $HOME/.cfg/env
-
-# Interactive cd plugin
-source $HOME/.zsh/zsh-interactive-cd.plugin.zsh
 
